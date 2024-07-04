86c533703887108274187ede8e2cb39485321c5d
조은영 <hdtv0218@gmail.com>
2024-07-05 01:20:46 +0900

    :spackles: 상세 공고로 페이지 이동

---
+++ frontend/lib/screens/ai_result_detail_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AiResultDetailScreen extends StatelessWidget {
+  const AiResultDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- frontend/lib/screens/ai_search_result_screen.dart
+++ frontend/lib/screens/ai_search_result_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:frontend/screens/ai_result_detail_screen.dart';
 import 'package:frontend/widgets/aiSearchProgress.dart';
 import 'package:frontend/models/ai_result/aI_result_dog.dart';
 import 'package:frontend/widgets/make_text_list.dart';
@@ -96,48 +97,57 @@ class AiSearchResultScreen extends StatelessWidget {
                   itemCount: resultDogs.length,
                   itemBuilder: (context, index) {
                     final dog = resultDogs[index];
-                    return Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(8),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Color.fromRGBO(0, 0, 0, 0.08),
-                            spreadRadius: 2,
-                            blurRadius: 28,
-                            offset: Offset(0, 4),
-                          ),
-                        ],
-                      ),
-                      child: Column(
-                        children: [
-                          AspectRatio(
-                            aspectRatio: 1.7,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(12.0),
-                              child: Image.asset(
-                                dog.image,
-                                fit: BoxFit.cover,
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const AiResultDetailScreen()));
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Color.fromRGBO(0, 0, 0, 0.08),
+                              spreadRadius: 2,
+                              blurRadius: 28,
+                              offset: Offset(0, 4),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          children: [
+                            AspectRatio(
+                              aspectRatio: 1.7,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(12.0),
+                                child: Image.asset(
+                                  dog.image,
+                                  fit: BoxFit.cover,
+                                ),
                               ),
                             ),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          MakeTextList(
-                            title: ' ${dog.location}에서 발견',
-                            textIcon: Icons.info_outline,
-                          ),
-                          MakeTextList(
-                            title: ' ${dog.dateTime}구조',
-                            textIcon: Icons.calendar_month_outlined,
-                          ),
-                          MakeTextList(
-                            title: ' ${dog.whoProtected}',
-                            textIcon: Icons.location_on_outlined,
-                          ),
-                        ],
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            MakeTextList(
+                              title: ' ${dog.location}에서 발견',
+                              textIcon: Icons.info_outline,
+                            ),
+                            MakeTextList(
+                              title: ' ${dog.dateTime}구조',
+                              textIcon: Icons.calendar_month_outlined,
+                            ),
+                            MakeTextList(
+                              title: ' ${dog.whoProtected}',
+                              textIcon: Icons.location_on_outlined,
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   },
